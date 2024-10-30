7be8b7042dee270a8ba10d3e9612b1c708c9a787
Rodrigo Debossans <rodrigo.debossans@gmail.com>
2024-10-30 20:26:31 -0300

    perf(analyze): add database to index words and improve performance

--- src/cli/commands/analyze.ts
+++ src/cli/commands/analyze.ts
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { DatabaseSync } from 'node:sqlite';
+import { statSync } from 'fs';
+import { resolve } from 'path';
 import TreeModel from 'tree-model';
 import { performance } from 'perf_hooks';
 import { Command, InvalidArgumentError } from 'commander';
 
+import { WapMetricLabels } from '../enums';
 import { WapDepthParser } from '../parsers/depth-parser';
 import { WapAnalyzeCommandOptions, WapNode } from '../models';
 import {
@@ -10,9 +15,10 @@ import {
   getAnalyzeOutput,
   getAnalyzeTableMetrics,
   getNoMatchingWordsMessage,
+  phraseFilterPredicate,
+  WapWordDatabaseHelper,
 } from '../helpers';
 import classificationTreeJson from './../../../dicts/classification-tree.json';
-import { WapMetricLabels } from '../enums';
 
 export class WapAnalyzeCommand extends Command {
   private parameterLoadingTimeStartLabel = `${WapMetricLabels.PARAMETER_LOADING_TIME}-start`;
@@ -42,6 +48,7 @@ export class WapAnalyzeCommand extends Command {
       .argument('<phrase>', 'text to be analyzed')
       .requiredOption('-d, --depth <value>', 'tree depth level for which to display the count', WapDepthParser.parse)
       .option('-n, --normalize', 'ignore special characters in words')
+      .option('-i, --index', 'index words in the database to improve performance')
       .option('-v, --verbose', 'display table with execution metrics')
       .hook('preAction', () => {
         performance.mark(this.parameterLoadingTimeEndLabel);
@@ -52,9 +59,7 @@ export class WapAnalyzeCommand extends Command {
         );
         performance.mark(this.phraseVerificationTimeStartLabel);
       })
-      .action((phrase, options) => {
-        this.execute(phrase, options);
-      })
+      .action(this.execute as any)
       .hook('postAction', () => {
         performance.mark(this.phraseVerificationEndLabel);
         performance.measure(
@@ -65,23 +70,63 @@ export class WapAnalyzeCommand extends Command {
       });
   }
 
-  execute(phrase: string, options: WapAnalyzeCommandOptions): string {
+  indexWordsInDatabase(database: DatabaseSync, tree: TreeModel.Node<WapNode>): void {
+    WapWordDatabaseHelper.createWordsTable(database);
+    WapWordDatabaseHelper.createFileMetadataTable(database);
+
+    const fileProps = {
+      filename: 'classification-tree.json',
+      path: resolve(__dirname, './../../../dicts/classification-tree.json'),
+    };
+
+    const classificationTreePath = resolve(__dirname, fileProps.path);
+    const treeFileStats = statSync(classificationTreePath);
+
+    const storageFileMetadata = WapWordDatabaseHelper.getFileMetadata({ database, filename: fileProps.filename });
+    const fileIsNotChanged = storageFileMetadata?.modificationTime === treeFileStats.mtimeMs;
+    if (fileIsNotChanged) return;
+
+    WapWordDatabaseHelper.updateFileMetadata({
+      database,
+      filename: fileProps.filename,
+      mtimeMs: treeFileStats.mtimeMs,
+    });
+
+    const allWordsInClassificationTree = tree
+      .all(node => !!node)
+      .map(node => ({ name: node.model.name, depth: node.getPath().length }));
+
+    WapWordDatabaseHelper.insertWords({
+      database,
+      table: 'words',
+      words: allWordsInClassificationTree,
+    });
+  }
+
+  async execute(phrase: string, options: WapAnalyzeCommandOptions): Promise<string> {
     if (!phrase) throw new InvalidArgumentError('Phrase is required');
 
     const treeModel = new TreeModel();
     const tree = treeModel.parse<WapNode>(classificationTreeJson);
 
+    let words = null;
+
+    if (options.index) {
+      const database = WapWordDatabaseHelper.openWordDatabase();
+      this.indexWordsInDatabase(database, tree);
+      const indexedWords = WapWordDatabaseHelper.getIndexedWords(database);
+      words = phrase.split(/\s+/).filter(phraseFilterPredicate(indexedWords));
+    } else words = phrase.split(/\s+/).filter(word => word.length > 0);
+
     const wordCountOutputMap: Map<string, number> = new Map();
-    const words = phrase.split(/\s+/).filter(word => word.length > 0);
     const nodesAtDepth = tree.all(node => node.getPath().length === options.depth);
 
     let totalNodesProcessed = 0;
 
-    for (const word of words) {
-      let findPredicate = findWordByNodeNamePredicate(word);
-      if (options.normalize) findPredicate = findWordByNodeNameWithoutAccentsPredicate(word);
+    const findPredicate = options.normalize ? findWordByNodeNameWithoutAccentsPredicate : findWordByNodeNamePredicate;
 
-      const matchingNode = nodesAtDepth.find(node => node.first(findPredicate));
+    for (const word of words) {
+      const matchingNode = nodesAtDepth.find(node => node.first(findPredicate(word)));
       if (!matchingNode) continue;
 
       const name = matchingNode?.model.name;
--- src/cli/helpers/index.ts
+++ src/cli/helpers/index.ts
@@ -1,12 +1,27 @@
+import sqlBricks from 'sql-bricks';
 import { table, TableUserConfig } from 'table';
-import { WapNodeModel } from '../models';
+import {
+  WapFileMetadata,
+  WapGetFileMetadataOptions,
+  WapIndexedWord,
+  WapInsertWordsOptions,
+  WapNodeModel,
+  WapUpdateWordsOptions,
+} from '../models';
+import { DatabaseSync } from 'node:sqlite';
+
+export const phraseFilterPredicate = (indexedWords: WapIndexedWord[]) => (word: string) =>
+  word.length > 0 &&
+  indexedWords.find(
+    indexedWord => removeAccents(indexedWord?.name.toLowerCase()) === removeAccents(word.toLowerCase()),
+  );
 
 export const findWordByNodeNamePredicate = (word: string) => (node: WapNodeModel) =>
   node.model.name.toLowerCase() === word.toLowerCase();
 
-export const findWordByNodeNameWithoutAccentsPredicate = (word: string) => (node: WapNodeModel) => {
-  const removeAccents = (str: string) => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+export const removeAccents = (str: string) => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
+export const findWordByNodeNameWithoutAccentsPredicate = (word: string) => (node: WapNodeModel) => {
   const nodeName = removeAccents(node.model.name.toLowerCase());
   const cleanWord = removeAccents(word.toLowerCase());
 
@@ -34,3 +49,43 @@ export const getAnalyzeTableMetrics = (parameterLoadingTime: number, phraseVerif
 
 export const getNoMatchingWordsMessage = (depth: number) =>
   `In the phrase there are no children of the ${depth} level and neither does the ${depth} level have the specified terms.`;
+
+export class WapWordDatabaseHelper {
+  static openWordDatabase(): DatabaseSync {
+    return new DatabaseSync('word-database.sqlite');
+  }
+
+  static createWordsTable(database: DatabaseSync): void {
+    database.exec('CREATE TABLE IF NOT EXISTS words (name TEXT NOT NULL, depth INTEGER NOT NULL)');
+  }
+
+  static createFileMetadataTable(database: DatabaseSync): void {
+    database.exec('CREATE TABLE IF NOT EXISTS file_metadata (filename PRIMARY KEY, modificationTime REAL NOT NULL)');
+  }
+  static getIndexedWords(database: DatabaseSync): WapIndexedWord[] {
+    const query = sqlBricks.select('name, depth').from('words').toString();
+
+    const selectStatement = database.prepare(query);
+    return selectStatement.all() as WapIndexedWord[];
+  }
+
+  static insertWords({ database, table, words }: WapInsertWordsOptions): void {
+    const { text, values } = sqlBricks.insertInto(table, words).toParams({ placeholder: '?' });
+    const insertStatement = database.prepare(text);
+    insertStatement.run(...values);
+  }
+
+  static getFileMetadata({ database, filename }: WapGetFileMetadataOptions): WapFileMetadata {
+    const query = sqlBricks.select('filename, modificationTime').from('file_metadata').where({ filename }).toString();
+
+    const selectStatement = database.prepare(query);
+    return selectStatement.get() as WapFileMetadata;
+  }
+
+  static updateFileMetadata({ database, filename, mtimeMs }: WapUpdateWordsOptions) {
+    const statement = database.prepare(
+      'INSERT OR REPLACE INTO file_metadata (filename, modificationTime) VALUES (?, ?)',
+    );
+    return statement.run(filename, mtimeMs);
+  }
+}
